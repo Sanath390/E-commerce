7361f5b40541fed49ce090230fa8c0b1aaf51787
Sanath390 <sanathgk19@gmail.com>
2024-10-04 20:13:35 +0530

    Added liner gradient to login page+

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './main/login';
+import { Box } from '@mui/material';
 
 function App() {
+  let linearGradient = 'linear-gradient(0.3turn, #73355C,#AC3028,#B78838,#254828,#4B8316)';
   return (
     <>
+    <Box 
+    sx={{display: 'flex', background: linearGradient}}>
     <Login/>
+    </Box>
     </>
   )
 }
